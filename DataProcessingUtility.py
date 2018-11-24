a69bdc49bfdf3cceec8d5996e9a3f5e973c8c59e
Thanh To <thanhto@kms.com.vn>
2018-11-25 00:05:54 +0700

    First working prototype

--- DataProcessingUtility.py
+++ DataProcessingUtility.py
@@ -4,18 +4,15 @@ import numpy as np
 import csv
 
 
-# write it
+# write it into CSV
 def writeMatrixToFile(matrix, fileName):
     with open(fileName, 'w') as csvfile:
         writer = csv.writer(csvfile)
         [writer.writerow(r) for r in matrix]
     
-# read it
+# read CSV and return a numpy array
 def readMatrixFromFile(fileName):
-    with open(fileName, 'r') as csvfile:
-        reader = csv.reader(csvfile)
-        table = [[int(e) for e in r] for r in reader]
-        return table
+    return np.genfromtxt(fileName, delimiter=',')
 
 def make_Dictionary(train_dir):
     emails = [os.path.join(train_dir,f) for f in os.listdir(train_dir)]    
@@ -58,62 +55,3 @@ def extract_features(mail_dir, dictionary):
                                 features_matrix[docID, wordID] = words.count(word)
         docID = docID + 1     
     return features_matrix
-
-
-def make_Dictionary1(root_dir):
-    emails_dirs = [os.path.join(root_dir, f) for f in
-                   os.listdir(root_dir)]
-    all_words = []
-    for emails_dir in emails_dirs:
-        dirs = [os.path.join(emails_dir, f) for f in
-                os.listdir(emails_dir)]
-        for d in dirs:
-            emails = [os.path.join(d, f) for f in os.listdir(d)]
-            for mail in emails:
-                with open(mail) as m:
-                    for line in m:
-                        words = line.split()
-                        all_words += words
-    dictionary = Counter(all_words)
-    list_to_remove = dictionary.keys()
-
-    for item in list_to_remove:
-        if item.isalpha() == False:
-            del dictionary[item]
-        elif len(item) == 1:
-            del dictionary[item]
-    dictionary = dictionary.most_common(3000)
-
-    np.save('dict_enron.npy', dictionary)
-
-    return dictionary
-
-
-def extract_features1(root_dir, dictionary):
-    emails_dirs = [os.path.join(root_dir, f) for f in
-                   os.listdir(root_dir)]
-    docID = 0
-    features_matrix = np.zeros((33716, 3000))
-    train_labels = np.zeros(33716)
-    for emails_dir in emails_dirs:
-        dirs = [os.path.join(emails_dir, f) for f in
-                os.listdir(emails_dir)]
-        for d in dirs:
-            emails = [os.path.join(d, f) for f in os.listdir(d)]
-            for mail in emails:
-                with open(mail) as m:
-                    all_words = []
-                    for line in m:
-                        words = line.split()
-                        all_words += words
-                    for word in all_words:
-                        wordID = 0
-                        for (i, d) in enumerate(dictionary):
-                            if d[0] == word:
-                                wordID = i
-                                features_matrix[docID, wordID] = \
-                                    all_words.count(word)
-                train_labels[docID] = int(mail.split('.')[-2] == 'spam')
-                docID = docID + 1
-    return (features_matrix, train_labels)
-    
--- Main.py
+++ Main.py
@@ -1,45 +1,46 @@
 import numpy as np
 import DataProcessingUtility as dataUtility
 import os.path
-from NeuralNetworkClassifier import NeuralNetworkClassifier
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.svm import LinearSVC
 from sklearn.metrics import confusion_matrix
 from sklearn.preprocessing import StandardScaler
 from pathlib import Path
 from sklearn.decomposition import PCA
+from sklearn.neural_network import MLPClassifier
 
-def readDataAndConstructFeatureMatrix():    
+def readData():    
     # Create a dictionary of words with its frequency
-    print("Create a dictionary of words with its frequency")
     train_dir = 'dataset/ling-spam/train-mails'
     dictionary = dataUtility.make_Dictionary(train_dir)
     
     # Prepare feature vectors per training mail and its labels
-    print("Prepare feature vectors per training mail and its labels")
     train_labels = np.zeros(702)
     train_labels[351:701] = 1
 
     # Check if feature matrix file already exists
     train_matrix_file = Path('train_matrix.csv')
     if os.path.isfile(train_matrix_file):
-        train_matrix = dataUtility.readMatrixFromFile('train_matrix.csv')
-    else:  
+        print("Reading training feature matrix from local cache")
+        train_matrix = np.array(dataUtility.readMatrixFromFile('train_matrix.csv'))
+    else:
+        print("Training feature matrix doesn't exist, creating and writing into local cache")
         train_matrix = dataUtility.extract_features(train_dir, dictionary)            
         # Write feature matrix to file
         dataUtility.writeMatrixToFile(train_matrix, 'train_matrix.csv')
 
     
     # Prepare test data and labels
-    print("Prepare feature vectors per testing mail and its labels")
     test_dir = 'dataset/ling-spam/test-mails'
     
     # Check if feature matrix file already exists
     train_matrix_file = Path('test_matrix.csv')
     if os.path.isfile(train_matrix_file):
-        test_matrix = dataUtility.readMatrixFromFile('test_matrix.csv')
+        print("Reading test feature matrix from local cache")
+        test_matrix = np.array(dataUtility.readMatrixFromFile('test_matrix.csv'))
     else:
-        test_matrix = dataUtility.extract_features(test_dir, dictionary)    
+        print("Training test matrix doesn't exist, creating and writing into local cache")
+        test_matrix = dataUtility.extract_features(test_dir, dictionary)
         # Write feature matrix to file
         dataUtility.writeMatrixToFile(test_matrix, 'test_matrix.csv')
     
@@ -48,59 +49,64 @@ def readDataAndConstructFeatureMatrix():
     return train_matrix, train_labels, test_matrix, test_labels
 
 
-
-def trainSVMAndNaiveBayes(train_matrix, train_labels):
-    # SVM and Naive Bayes 
-    print("Training SVM and Naive Bayes classifier and its variants")
-    model1 = LinearSVC()
-    model2 = MultinomialNB()
-    
-    model1.fit(train_matrix,train_labels)
-    model2.fit(train_matrix,train_labels)
-    return model1, model2
-
-
-
 if __name__ == '__main__':
     
-    train_matrix, train_labels, test_matrix, test_labels = readDataAndConstructFeatureMatrix()
+    train_matrix, train_labels, test_matrix, test_labels = readData()
     
     print("Training SVM and Naive Bayes model")
     # Train SVM and Naive Bayes
-    model1, model2 = trainSVMAndNaiveBayes(train_matrix, train_labels)
-    
-    print("Perform PCA on training feature matrix")
-    # Perform dimensionality reduction on train_matrix with PCA
-    pca1 = PCA(0.90)
-    normalized_train_matrix = StandardScaler().fit_transform(train_matrix)
-    pca1.fit(normalized_train_matrix)
-    reduced_train_matrix = pca1.transform(normalized_train_matrix)
-    print("Number of features before reduction", len(train_matrix[0]))
-    print("Number of features after reduction", len(reduced_train_matrix[0]))
+    print("Training SVM and Naive Bayes classifier")
+    model1 = LinearSVC()
+    model2 = MultinomialNB()    
+    model1.fit(train_matrix,train_labels)
+    model2.fit(train_matrix,train_labels)
     
     # Train Neural network
-    print("Training Neural Network on reduced feature matrix")
-    model3 = NeuralNetworkClassifier(reduced_train_matrix, train_labels, 1000)
-    model3.trainNeuralNetwork()
-    
-    print("Predict with SVM and Naive Bayes")
+    print("Training Neural Network")
+    model3 = MLPClassifier()
+    model3.fit(train_matrix, train_labels)
+
     # Predict with SVM and Naive Bayes
     result1 = model1.predict(test_matrix)
     result2 = model2.predict(test_matrix)
+    result3 = model3.predict(test_matrix)
     
-    # Predict with Neural Network
+    print("Perform PCA to reduce dimensionality")
+    # Perform dimensionality reduction on train_matrix with PCA
+    # PCA will choose N features such that more than 80% of the variance are explained
+    pca1 = PCA(0.85)
+    normalized_train_matrix = StandardScaler().fit_transform(train_matrix)
+    pca1.fit(normalized_train_matrix)
+    reduced_train_matrix = pca1.transform(normalized_train_matrix)
+    
+    print("Number of features before reduction", len(train_matrix[0]))
+    print("Number of features after reduction", len(reduced_train_matrix[0]))
+
     pca2 = PCA(len(reduced_train_matrix[0]))
     normalized_test_matrix = StandardScaler().fit_transform(test_matrix)
     pca2.fit(normalized_test_matrix)
     reduced_test_matrix = pca2.transform(normalized_test_matrix)
-    print("Predict with Neural Network")
-    result3 = model3.predict(test_matrix)
+    
+    print("Training SVM and Neural Network on the reduced feature space")
+    # After reduction
+    model1.fit(reduced_train_matrix,train_labels)
+    model3.fit(reduced_train_matrix,train_labels)
+    
+    result4 = model1.predict(reduced_test_matrix)
+    result6 = model3.predict(reduced_test_matrix)
     
     # Result
+    print("---------- Original feature space -------------")
     print("Confusion matrix for SVM")
     print(confusion_matrix(test_labels, result1))
     print("Confusion matrix for Naive Bayes")
     print(confusion_matrix(test_labels, result2))
     print("Confusion matrix for Neural network")
     print(confusion_matrix(test_labels, result3))
+    print("---------- Reduced feature space -------------")
+    print("Confusion matrix for SVM")
+    print(confusion_matrix(test_labels, result4))
+    print("Confusion matrix for Neural network")
+    print(confusion_matrix(test_labels, result6))
+    
     pass
